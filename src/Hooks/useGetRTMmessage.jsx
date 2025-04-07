30313b011209f3234fa31ee93a7a554f22285b20
CoderRaushan <raushankumar23082004@gmail.com>
2025-04-07 16:21:15 +0530

    changes made in usehook RTM to slove bug to get sending msg on time

--- src/Hooks/useGetRTMmessage.jsx
+++ src/Hooks/useGetRTMmessage.jsx
@@ -1,18 +1,44 @@
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setMessages } from "../ReduxStore/ChatSlice.js";
+// function useGetRTMmessage() {
+//   const { socket } = useSelector((store) => store.socketio);
+//   const { messages } = useSelector((store) => store.chat);
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     socket?.on("newMessage", (newmsg) => {
+//       dispatch(setMessages([...messages, newmsg]));
+//     });
+//     return () => {
+//       socket?.off("newMessage");
+//     };
+//   }, [socket, dispatch]);
+// }
+
+// export default useGetRTMmessage;
+
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setMessages } from "../ReduxStore/ChatSlice.js";
 function useGetRTMmessage() {
   const { socket } = useSelector((store) => store.socketio);
-  const { messages } = useSelector((store) => store.chat);
   const dispatch = useDispatch();
+  const messagesRef = React.useRef([]);
+
+  const messages = useSelector((store) => store.chat.messages);
   useEffect(() => {
-    socket?.on("newMessage", (newmsg) => {
-      dispatch(setMessages([...messages, newmsg]));
-    });
+    messagesRef.current = messages;
+  }, [messages]);
+
+  useEffect(() => {
+    const handleNewMessage = (newmsg) => {
+      dispatch(setMessages([...messagesRef.current, newmsg]));
+    };
+
+    socket?.on("newMessage", handleNewMessage);
     return () => {
-      socket?.off("newMessage");
+      socket?.off("newMessage", handleNewMessage);
     };
   }, [socket, dispatch]);
 }
-
 export default useGetRTMmessage;
